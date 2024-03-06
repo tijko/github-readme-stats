7aa04e95b1cd22c6dd89b7b3da2d4e976bb97c70
Tim Konick <konick781@gmail.com>
2024-03-05 19:47:00 -0500

    Remove ENV ci

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -7,6 +7,6 @@ git config --global user.email "konick781@gmail.com"
 git config --global user.name "tijko"
 git config --global --add safe.directory /github/workspace
 git add -A
-git commit -m "Update theme readme" || exit 0
+git commit -m "Run README-Github-Theme creation scripts" || exit 0
 git remote add origin https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
 git push origin master 
