419ceb4240bd20deb6ed58c39d5279ae7672a2f0
Tim Konick <konick781@gmail.com>
2024-03-05 19:17:02 -0500

    Broken switch "a"

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -6,7 +6,7 @@ git --version
 git config --global user.email "konick781@gmail.com"
 git config --global user.name "tijko"
 git config --global --add safe.directory /github/workspace
-git add -a
+git add -A
 git commit -m "Update theme readme" || exit 0
 git remote add origin https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
 git push origin master 
