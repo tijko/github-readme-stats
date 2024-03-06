4cc4c6672158eaefbccb66086085d46e1686c523
Tim Konick <konick781@gmail.com>
2024-03-05 19:11:16 -0500

    Add safe-directory

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -6,6 +6,7 @@ export BRANCH_NAME=master
 git --version
 git config --global user.email "konick781@gmail.com"
 git config --global user.name "tijko"
+git config --global --add safe.directory /github/workspace
 git checkout -b $BRANCH_NAME
 git add -a
 git commit -m "Update theme readme" || exit 0
