ea9be885f6ac3093cc16c5f62d2d390ecaad964a
Tim Konick <konick781@gmail.com>
2024-03-05 19:12:44 -0500

    Fix the checkout line

--- scripts/push-theme-readme.sh
+++ scripts/push-theme-readme.sh
@@ -2,13 +2,11 @@
 set -x
 set -e
 
-export BRANCH_NAME=master
 git --version
 git config --global user.email "konick781@gmail.com"
 git config --global user.name "tijko"
 git config --global --add safe.directory /github/workspace
-git checkout -b $BRANCH_NAME
 git add -a
 git commit -m "Update theme readme" || exit 0
 git remote add origin https://${PERSONAL_TOKEN}@github.com/${GH_REPO}.git
-git push --force --quiet origin master 
+git push origin master 
